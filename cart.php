d1035fd0be6072d2b3cf274691e55a61f35ea99e
Simona Poliakova <131616907+SimonaPoliakova@users.noreply.github.com>
2023-05-12 20:53:13 +0200

    cart page php

--- cart.php
+++ cart.php
@@ -1,3 +1,109 @@
+<?php 
+
+session_start();
+
+  //calculate total
+  calculateTotalCart();
+
+if(isset($_POST["add_to_cart"])) {
+
+  //if product was already added to cart
+  if(isset($_SESSION["cart"])) {
+
+    $products_array_ids = array_column($_SESSION["cart"],"product_id");
+    //if product has been added to cart or not
+    if(!in_array($_POST["product_id"], $products_array_ids)) {
+    
+      $product_id = $_POST["product_id"];
+
+        $product_array = array(
+          "product_id" => $_POST["product_id"],
+          "product_name" =>$_POST["product_name"],
+          "product_price" => $_POST["product_price"],
+          "product_image" => $_POST["product_image"],
+          "product_quantity" => $_POST["product_quantity"]
+      );
+  
+     $_SESSION["cart"][$_POST["product_id"]] = $product_array; 
+
+      //if product has already been added
+    } else {
+      echo "<script>alert('Product was already added to cart');</script>";
+    }
+ 
+  //if this is the first product
+  } else {
+
+      $product_id = $_POST["product_id"];
+      $product_name = $_POST["product_name"];
+      $product_price = $_POST["product_price"];
+      $product_image = $_POST["product_image"];
+      $product_quantity = $_POST["product_quantity"];
+
+      $product_array = array(
+        "product_id" => $product_id,
+        "product_name" => $product_name,
+        "product_price" => $product_price,
+        "product_image" => $product_image,
+        "product_quantity" => $product_quantity
+    );
+
+   $_SESSION["cart"][$product_id] = $product_array; 
+   // special id to each array
+  }
+
+//remove from cart
+}else if(isset($_POST["remove_product"])){
+
+  $product_id = $_POST["product_id"];
+  unset($_SESSION["cart"][$product_id]);
+
+  //calculate total
+  calculateTotalCart();
+
+
+}else if(isset($_POST["edit_quantity"])){ 
+  //get id and quantity from the form
+  $product_id = $_POST["product_id"];
+  $product_quantity = $_POST["product_quantity"];
+  
+  //get product array from the session
+  $product_array = $_SESSION["cart"][$product_id];
+
+  //old_quantity = new quantity
+  $product_array["product_quantity"] = $product_quantity;
+
+  $_SESSION["cart"][$product_id] = $product_array; 
+
+  //calculate total
+  calculateTotalCart();
+
+
+} else {
+  header("location: index.php");
+}
+
+function calculateTotalCart(){
+
+  $total = 0;
+  foreach($_SESSION["cart"] as $key => $value){
+
+    $product = $_SESSION["cart"][$key];
+    
+    $price = $product["product_price"];
+    $quantity = $product["product_quantity"];
+
+    $total = $total + ($price * $quantity);
+
+  }
+
+  $_SESSION["total"] = $total;
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -59,41 +165,48 @@
 </nav>
 
 <!--cart-->
-<div class="cart">
+<main>
+<div class="cart" id="cart">
     <h2>Your Cart</h2>
-    <div class="cart-items">
+     <div class="cart-items">
+
+    <?php foreach($_SESSION["cart"] as $key => $value) { ?>
+
       <div class="cart-row">
         <div class="cart-item cart-column">
-          <img class="cart-item-image" src="assets/img/featured1.jpg" alt="item">
-          <span class="cart-item-title">Product Name</span>
+          <img class="cart-item-image" src="assets/img/<?php echo $value['product_image']; ?>" alt="item">
+          <span class="cart-item-title"><?php echo $value['product_name']; ?></span>
         </div>
-        <span class="cart-price cart-column">$19.99</span>
+        <span class="cart-price cart-column">€<?php echo $value['product_price']; ?></span>
         <div class="cart-quantity cart-column">
-          <input class="cart-quantity-input" type="number" value="1">
-          <button class="btn btn-danger" type="button">REMOVE</button>
-          <button class="btn btn-edit" type="button">EDIT</button>
+         
+          
+          <form method="POST" action="cart.php">
+           <input type="hidden" name="product_id" value="<?php echo $value['product_id']; ?>">
+           <input type="submit" name="remove_product" class="btn btn-danger" value="remove">
+          </form> 
+
+          <form method="POST" action="cart.php">
+            <input class="btn-edit" value="edit" name="edit_quantity" class="btn btn-edit" type="submit">
+            <input class="cart-quantity-input" name="product_quantity" type="number" value="<?php echo $value["product_quantity"];?>">
+            <input type="hidden" name="product_id" value="<?php echo $value["product_id"]?>">
+          </form>
+
         </div>
       </div>
-      <div class="cart-row">
-        <div class="cart-item cart-column">
-          <img class="cart-item-image" src="assets/img/featured2.jpg" alt="item">
-          <span class="cart-item-title">Product Name</span>
-        </div>
-        <span class="cart-price cart-column">$19.99</span>
-        <div class="cart-quantity cart-column">
-          <input class="cart-quantity-input" type="number" value="1">
-          <button class="btn btn-danger" type="button">REMOVE</button>
-          <button class="btn btn-edit" type="button">EDIT</button>
-        </div>
-      </div>
+
+      <?php } ?>
+      
       <div class="cart-total">
         <strong class="cart-total-title">Total</strong>
-        <span class="cart-total-price">$39.98</span>
+        <span class="cart-total-price">€<?php echo $_SESSION["total"]; ?></span>
       </div>
-      <button class="btn btn-primary btn-purchase" type="button">CHECKOUT</button>
+      <form method="POST" action="checkout.php">
+         <input class="btn btn-primary btn-purchase" type="submit" value="CHECKOUT" name="checkout">
+      </form>
     </div>
   </div>
-  
+    </main>
   
     <!--footer-->
     <footer id="footer">
--- single_product.php
+++ single_product.php
@@ -2,6 +2,7 @@
 
 include("server/connection.php");
 
+//get single product php
 if(isset($_GET["product_id"])){
 
     $product_id = $_GET["product_id"];
@@ -83,6 +84,7 @@ if(isset($_GET["product_id"])){
     <div class="row mt-5">
 
     <?php while($row = $product->fetch_assoc()){ ?>
+
         <div class="col-lg-5 col-md-6 col-sm-12">
             <img class="img-fluid w-100 pb-1" src="assets/img/<?php echo $row["product_image"]; ?>" id="mainImg">
             <div class="small-img-group">
@@ -105,8 +107,19 @@ if(isset($_GET["product_id"])){
             <h6>Shoes</h6>
             <h3 class="py-4"><?php echo $row["product_name"]; ?></h3>
             <h2>€<?php echo $row["product_price"]; ?></h2>
-            <input type="number" value="1">
-            <button class="buy-btn">Add To Cart</button> 
+            
+            <form method="POST" action="cart.php">
+                <input type="hidden" name="product_id" value="<?php echo $row["product_id"]; ?>">
+                <input type="hidden" name="product_image" value="<?php echo $row["product_image"]; ?>">
+                <input type="hidden" name="product_name" value="<?php echo $row["product_name"]; ?>">
+                <input type="hidden" name="product_price" value="<?php echo $row["product_price"]; ?>">
+
+            <input type="number" name="product_quantity" value="1">
+            <button class="buy-btn" type="submit" name="add_to_cart">Add To Cart</button> 
+
+            </form>
+
+           
             <h4 class="mt-5 mb-5">Product details</h4>
             <span><?php echo $row["product_description"]; ?></span>
         </div>
