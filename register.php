f7a998c126f456bdb48664ad941008fbb86c5f44
Simona Poliakova <131616907+SimonaPoliakova@users.noreply.github.com>
2023-05-13 22:19:03 +0200

    getting user id

--- register.php
+++ register.php
@@ -49,6 +49,8 @@ else {
 
             //account was created
             if($stmt->execute()){
+                $user_id = $stmt->insert_id;
+                $_SESSION["user_id"] = $user_id;
                 $_SESSION["user_email"] = $email;
                 $_SESSION["user_name"] = $name;
                 $_SESSION["logged_in"] = true;
