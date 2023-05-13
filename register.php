cfd1ad53ec410db0b6336e56e9de0f53bf1232f6
Simona Poliakova <131616907+SimonaPoliakova@users.noreply.github.com>
2023-05-13 17:35:33 +0200

    registration

--- register.php
+++ register.php
@@ -1,3 +1,72 @@
+<?php 
+
+session_start();
+include("server/connection.php");
+
+if(isset($_POST["register"])){
+
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $confirmPassword = $_POST["confirmPassword"];
+
+        //if passwords match
+        if($password !== $confirmPassword){
+            header("location: register.php?error=Passswords do not match");
+        }
+
+        //if the  password is shorter than 6
+        else if(strlen($password) < 6){
+            header("location: register.php?error=Password must be at least 6 characters long");
+    }
+
+//if there is no error
+else {
+        //check if there is user with this email or not
+        $stmt1 = $conn->prepare("SELECT count(*) FROM users WHERE user_email=?");
+        $stmt1->bind_param("s", $email);
+        $stmt1->execute();
+        $stmt1->bind_result($num_rows);
+        $stmt1->store_result();
+        $stmt1->fetch();
+
+        if($num_rows != 0){
+            header("location: register.php?error=User with this email already exists");
+        }
+
+        else{
+            //create new user
+            $stmt = $conn->prepare("INSERT INTO users(user_name, user_email, user_password)
+            VALUES(?, ?, ?)");
+
+            $stmt->bind_param("sss", $name, $email, md5($password));
+
+            //account was created
+            if($stmt->execute()){
+                $_SESSION["user_email"] = $email;
+                $_SESSION["user_name"] = $name;
+                $_SESSION["logged_in"] = true;
+                header("location: account.php?register=Registration was successful");
+            //account could not be created
+            }else{
+                header("location: register.php?error=Could not create an account");
+            }
+        }      
+    }
+//if user is already registered, take him to account page
+}else if(isset($_SESSION["logged_in"])){
+    header("location: account.php");
+    exit;
+}
+
+
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -66,7 +135,8 @@
     </div>
 
     <div class="mx-auto container">
-        <form id="register-form">
+        <form id="register-form" method="POST" action="register.php">
+            <p style="color: red;"><?php if(isset($_GET["error"])){ echo $_GET["error"]; }?></p>
 
             <div class="form-group">
                 <label>Name</label>
@@ -85,15 +155,15 @@
 
             <div class="form-group">
                 <label>Confirm password</label>
-                <input type="text" class="form-control" id="register-confirm-password" name="confirmPassword" placeholder="ConfirmPassword" required>
+                <input type="password" class="form-control" id="register-confirm-password" name="confirmPassword" placeholder="ConfirmPassword" required>
             </div>
 
             <div class="form-group">
-                <input type="submit" class="btn" id="register-btn" value="Register">
+                <input type="submit" class="btn" id="register-btn" value="Register" name="register">
             </div>
 
             <div class="form-group">
-               <a id="login-url" class="btn">Do you have an account? Log in</a>
+               <a id="login-url" href="login.php" class="btn">Do you have an account? Log in</a>
             </div>
 
         </form>
