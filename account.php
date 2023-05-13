d07e9cce6c485faf00eeef75936be15670d4eb3c
Simona Poliakova <131616907+SimonaPoliakova@users.noreply.github.com>
2023-05-13 21:27:42 +0200

    logout

--- account.php
+++ account.php
@@ -7,6 +7,16 @@ if(!isset($_SESSION["logged_in"])){
     exit;
 }
 
+if(ISSET($_GET["logout"])){
+    if(isset($_SESSION["logged_in"])){
+        unset($_SESSION["logged_in"]);
+        unset($_SESSION["user_email"]);
+        unset($_SESSION["user_name"]);
+        header("location: login.php");
+        exit;    
+    }
+}
+
 ?>
 
 
@@ -80,7 +90,7 @@ if(!isset($_SESSION["logged_in"])){
                 <p>Name: <span><?php if(isset($_SESSION["user_name"])){echo $_SESSION["user_name"];} ?></span></p>
                 <p>Email: <span><?php if(isset($_SESSION["user_email"])){echo $_SESSION["user_email"];} ?></span></p>
                 <p><a href="" id="orders-btn">Your orders</a></p>
-                <p><a href="" id="logout-btn">Logout</a></p>
+                <p><a href="account.php?logout=1" id="logout-btn">Logout</a></p>
             </div>
         </div>
 
